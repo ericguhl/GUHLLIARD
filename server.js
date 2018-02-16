98180e3bb8e44ab7f0c59b5adf82d6bd5eec9e5e
Eric Guhl <ericguhl11@gmail.com>
2018-02-15 22:01:52 -0800

    mfo

--- server.js
+++ server.js
@@ -7,9 +7,49 @@ var port = 8080;
 app.set('view engine', 'ejs');
 // app.use(expressLayouts);
 
+
+
 // route our app
-var router = require('./app/routes');
-app.use('/', router);
+// var router = require('./app/routes');
+// app.use('/', router);
+
+
+
+
+
+// route for homepage
+app.get('/', function(req, res) {
+  res.status(200).render('pages/index');
+  // res.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+// route for rudiments
+app.get('/rudiments', function(req, res) {
+  res.status(200).render('pages/rudiments');
+});
+
+// route for almanac
+app.get('/almanac', function(req, res) {
+  res.status(200).render('pages/almanac');
+});
+
+// route for advanced
+app.get('/advanced', function(req, res) {
+  res.status(200).render('pages/advanced');
+});
+
+// route for about
+app.get('/about', function(req, res) {
+  res.status(200).render('pages/about');
+});
+
+// route for advanced
+app.get('/inspiration', function(req, res) {
+  res.status(200).render('pages/inspiration');
+});
+
+
+
 
 // set static files
 app.use(express.static(__dirname + '/public'));
